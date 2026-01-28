3f4b433f8e78869be953b3cf638859136e1061fc
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2026-01-28 22:59:32 +0530

    added userRoutes

--- affiliate-server/src/routes/userRoutes.js
+++ affiliate-server/src/routes/userRoutes.js
@@ -1,1 +1,14 @@
+const express= require('express');
+const userController=require("../controller/userController");
+const authMiddleware = require("../middleware.authMiddleWare");
 
+const router = express.Router();
+
+router.user(authMiddleware);
+
+router.post("/", userController.create);
+router.get("/", userController.getAll);
+router.put("/:id", userController.update);
+router.delete("/:id", userController.delete);
+
+module.exports = router;
