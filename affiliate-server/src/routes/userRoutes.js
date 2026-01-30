22c7daa47fd5e3171d52600e5b5896f03a01548e
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2026-01-30 09:35:34 +0530

    updated the routes by implementing permissions according to role

--- affiliate-server/src/routes/userRoutes.js
+++ affiliate-server/src/routes/userRoutes.js
@@ -1,14 +1,15 @@
 const express= require('express');
 const userController=require("../controller/userController");
 // const authMiddleware = require("../middleware.authMiddleWare");
+const authorize = require("../middleware/authorizedMiddleware");
 
 const router = express.Router();
 
 // router.user(authMiddleware);
 
-router.post("/", userController.create);
-router.get("/", userController.getAll);
-router.put("/:id", userController.update);
-router.delete("/:id", userController.delete);
+router.post("/", authorize('user: create'), userController.create);
+router.get("/", authorize('user: read'), userController.getAll);
+router.put("/:id", authorize('user: update'), userController.update);
+router.delete("/:id", authorize('user: delete'), userController.delete);
 
 module.exports = router;
